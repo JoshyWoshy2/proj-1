c2e97e5828099a97bdd12dfad6d237c8b6539594
Joshua Andreas <josh@Joshuas-MacBook-Pro.local>
2022-11-09 13:40:12 -0700

    nearly worked out

--- js/script.js
+++ js/script.js
@@ -144,49 +144,64 @@ let mistakes = 0;
 let guessed = [];
 let wordStatus = null;
 let pickedWord = theChosenOne.split("");
-document.getElementById("word").innerHTML = wordDisplay();
+let currentWord = wordDisplay();
 //EventListener
 document.addEventListener("click", handleClick);
 //137 words
+function display() {
+  document.getElementById("word").innerHTML = currentWord.join("");
+}
 function wordPicker() {
   let number = Math.floor(Math.random() * 137);
   return wordList[number];
 }
+display(wordDisplay());
 
 function wordDisplay() {
-  return pickedWord.map((char) => char.replace(/\S/g, "_")).join("");
+  return pickedWord.map((char) => char.replace(/\S/g, "_"));
 }
-
+console.log({ pickedWord });
+console.log({ currentWord });
 function handleClick(event) {
   //   console.log(event.target);
   if (event.target.classList.contains("letter")) {
     //1 value of clicked div = guessed
-    console.log("here1");
+    // console.log("here1");
     guessed = event.target.innerHTML.toLowerCase();
+    console.log(guessed);
     //2 compare to theChosenOne
-    //3 if matches character in answer
     if (pickedWord.includes(guessed)) {
+      //3 if matches character in answer
       ////1 show character location in word display
+      letterIdx = pickedWord.indexOf(guessed);
+      currentWord.splice(letterIdx, 1, guessed);
+      display();
       ////2 check if word is complete
-      //4 if no match
-      console.log({ guessed });
+      if (currentWord.join("") === theChosenOne) {
+        document.getElementById("text").innerHTML =
+          "Congatulations! You've saved our pirate pal!";
+      }
     } else if (pickedWord.indexOf(guessed) === -1) {
+      // console.log({ guessed });
+
+      //4 if no match
       ////1 mistakes++
       mistakes++;
       ////2 check if game lost
-      console.log(mistakes);
+      // console.log(pickedWord.indexOf(guessed));
+      // console.log(mistakes);
       if (mistakes === maxWrong) {
         document.getElementById("text").innerHTML =
           "He's dead, the answer was " + theChosenOne;
       }
       ////3 hang him a lil
+      //1 reset
     }
   } else if (event.target.matches("button")) {
-    //1 reset
     mistakes = 0;
     guessed = [];
   }
 }
-console.log(mistakes);
+// console.log(mistakes);
 // console.log(theChosenOne);
 // console.log(wordDisplay());
